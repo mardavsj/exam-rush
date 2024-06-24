2c2fc7dd2ee83fdc06a2417d818e3e3f8ef5a6c1
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-06-24 17:25:55 +0530

    size

--- components/footer.js
+++ components/footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
       <div className="relative">
         <div className="container mx-auto py-36">
           <div className="flex flex-wrap">
-            <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+            <div className="w-1/4 px-4">
               <div className="wow fadeInLeft" data-wow-delay="0.2s">
                 <h3 className="text-lg font-semibold text-gray-800 mb-6">Get in Touch</h3>
                 <p className="text-gray-600 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -24,7 +24,7 @@ const Footer = () => {
                 </form>
               </div>
             </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+            <div className="w-1/4 px-4">
               <div className="wow fadeInLeft" data-wow-delay="0.4s">
                 <h3 className="text-lg font-semibold text-gray-800 mb-6">Trending</h3>
                 <ul className="list-none space-y-3">
@@ -37,7 +37,7 @@ const Footer = () => {
                 </ul>
               </div>
             </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+            <div className="w-1/4 px-4">
               <div className="wow fadeInLeft" data-wow-delay="0.6s">
                 <h3 className="text-lg font-semibold text-gray-800 mb-6">Help</h3>
                 <ul className="list-none space-y-3">
@@ -50,7 +50,7 @@ const Footer = () => {
                 </ul>
               </div>
             </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+            <div className="w-1/4 px-4">
               <div className="wow fadeInLeft">
                 <h3 className="text-lg font-semibold text-gray-800 mb-6">Follow us at :</h3>
                 <div className="flex space-x-4">
@@ -66,10 +66,10 @@ const Footer = () => {
       </div>
       <div className="bg-gradient-to-tr from-purple-950 via-purple-700 to-purple-500 py-8">
         <div className="container mx-auto flex flex-wrap justify-between items-center">
-          <div className="w-full lg:w-1/2 text-white text-sm">
+          <div className="w-1/2 text-white text-sm">
             © ExamRush 2024 All rights reserved.
           </div>
-          <div className="w-full lg:w-1/2 text-right text-white text-sm">
+          <div className="w-1/2 text-right text-white text-sm">
             Made with <span>🤍</span>
           </div>
         </div>
