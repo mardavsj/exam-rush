e27a76cdaa42c2d3d26494247cd6d6f3e10f53ca
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-06-25 13:48:41 +0530

    head

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import HomePage from './HomePage'
+import Head from 'next/head'
 
 export default function Home() {
 
     return (
         <>
+            <div>
+                <Head>
+                    <title>Exam Rush</title>
+                </Head>
+            </div>
             <HomePage />
         </>
     )
