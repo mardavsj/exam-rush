b57088059713bc2f1b080941f6f9d1f901fd4361
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-06-28 17:58:53 +0530

    added google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                <link href="https://fonts.googleapis.com/css2?family=Neuton:wght@400&family=Oswald:wght@700&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
