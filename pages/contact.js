00694930e332b2092999f07d463df19c6a315260
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-06-30 17:17:16 +0530

    navbar updated

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Head from 'next/head'
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <div>
+      <Head>
+        <title>Contact Us - Clear every doubt | Exam Rush</title>
+      </Head>
+    <div className="p-36">contact</div>
+    </div>
   )
 }
 
---
+++ pages/games.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+const games = () => {
+  return (
+    <div>
+      <Head>
+        <title>Play & Learn - Coding Games | Exam Rush</title>
+      </Head>
+    <div className="p-36">games</div>
+    </div>
+  )
+}
+
+export default games
