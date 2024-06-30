4f1af603dc977fec724c83bf31c724b15fc20cdc
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-06-30 17:16:27 +0530

    position changed

--- content/pythonTheory.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const pythonTheory = () => {
-  return (
-    <div>pythonTheory</div>
-  )
-}
-
-export default pythonTheory
--- pages/hotquestions.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const hotquestions = () => {
-  return (
-    <div>hotquestions</div>
-  )
-}
-
-export default hotquestions
