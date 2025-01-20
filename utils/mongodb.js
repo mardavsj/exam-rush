0d99b236fedd526568b35ecd070d61a366bb765d
Mardav Jadaun <mardavjadaun113@gmail.com>
2025-01-20 16:13:06 +0530

    database connected

---
+++ utils/mongodb.js
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+let client;
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
